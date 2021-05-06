0adb49890f82c59946e935b12967a1a28c38eafb
Marc Solís <36197923+MarcSolis@users.noreply.github.com>
2021-05-06 20:23:45 +0200

    EnemyModel

---
+++ MagicTouch/MagicTouch/Models/BaloonModel.swift
@@ -0,0 +1,15 @@
+//
+//  BaloonModel.swift
+//  MagicTouch
+//
+//  Created by Alumne on 6/5/21.
+//
+
+import Foundation
+import UIKit
+
+struct baloon{
+    var color : UIColor
+    var movementId : Int
+    var hidden : Bool
+}
---
+++ MagicTouch/MagicTouch/Models/EnemyModel.swift
@@ -0,0 +1,14 @@
+//
+//  EnemyModel.swift
+//  MagicTouch
+//
+//  Created by Alumne on 6/5/21.
+//
+
+import Foundation
+
+struct EnemyModel{
+    var baallons : [baloon]
+    var skinId : Int
+    var points : Int
+}
