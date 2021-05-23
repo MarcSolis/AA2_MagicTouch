27d635732f76f69832d4d46fb8a56b757da8e47f
Arnau <ares.fernandez@enti.cat>
2021-05-23 11:52:38 +0200

    New Scene created

---
+++ MagicTouch/MagicTouch/MainMenu.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenu.swift
+//  MagicTouch
+//
+//  Created by Alumne on 23/5/21.
+//
+
+import Foundation
+import GameKit
+
+class MainMenu: SKScene, SKPhysicsContactDelegate {
+    
+}
